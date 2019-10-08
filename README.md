6fd70772fc7b34f2601776f1e4bd225b5d115b22
morralex <morr.alex@outlook.com>
2019-10-08 15:47:12 -0700

    finished gif

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # gifTastic
+
+This is an application that demonstrates the usage of API's
+
+* first we have an array of buttons that when called upon, searches the Giphy API and places gifs related to the button on the page.
+ * our second feature lets you create your own buttons and place those gifs on the page when the new button is clicked.
--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,56 @@
-$('#submit').on('click', function(e) {
+const btnArray = ["Cats", "Music", "Game of Thrones", "David Bowie", "Seinfield", "Coding", "Simpsons"]
+
+btnArray.forEach(function (name) {
+    $("#prompt").append("<button class='blah'>" + name + "</button>")
+})
+$('.blah').on('click', function (e) {
+    e.preventDefault();
+    // console.log(e)
+    const input = this.innerText
+    console.log(input)
+
+    axios.get('http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=whXzdI25neJRlcvNEGsfKSdWNrcCJPpp&limit=10')
+        .then(function (response) {
+            const gifs = response.data.data;
+            // console.log(gifs)
+            gifs.forEach(function (gif) {
+                // console.log(gif.images.original.url)
+                const mvg = gif.images.original.url;
+                // console.log(mvg);
+                $('#gifsgohere').prepend('<img src="' + mvg + '">')
+            })
+
+        })
+
+})
+
+
+$('#submit').on('click', function (e) {
     e.preventDefault();
     var userInput = $('#user-input').val()
-    axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + userInput)
-    .then(function (response){
-        console.log(response.data)
-        $('.container').append(response.data.Title)
+    console.log(userInput)
+
+    $('#new-btn').append("<button class='scndSet'>" + userInput + "</button>")
+
+    $('.scndSet').on('click', function (i) {
+        i.preventDefault();
+        // console.log(e)
+        const intxt = this.innerText
+        console.log(intxt)
+        axios.get('http://api.giphy.com/v1/gifs/search?q=' + userInput + '&api_key=whXzdI25neJRlcvNEGsfKSdWNrcCJPpp&limit=10')
+            .then(function (response) {
+                console.log(response.data)
+                const gifs = response.data.data;
+                // console.log(gifs)
+                gifs.forEach(function (gif) {
+                    // console.log(gif.images.original.url)
+                    const mvg = gif.images.original.url;
+                    // console.log(mvg);
+                    $('#gifsgohere').prepend('<img src="' + mvg + '">')
+                })
+            })
     })
-    .catch(function (err) {
-        console.error(err)
-    })
+
+
 })
+
